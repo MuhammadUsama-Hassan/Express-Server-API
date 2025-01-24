4d12ccbac06aa723206febaa7d3d02c885e3ff59
usama hassan <usama.hassan@xpertvault.com>
2025-01-24 21:48:44 +0500

    some modifications

--- expressJs/index.js
+++ expressJs/index.js
@@ -64,12 +64,3 @@ app.post("/api/users", (req, res) => {
 // Start the server
 app.listen(8000, () => log("Server Started on port 8000"));
 
-let myDate = new Date();
-log(myDate.toLocaleString());
-
-const myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-myArray.push(10, 11, 12);
-console.log(myArray);
-
-
-const newarray = myArray.map(()=>)
